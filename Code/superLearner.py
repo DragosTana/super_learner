8b41ebcd8a17cf8bf866fa68ef4846bcb10d36ca
Dragos <dragos.tanasa99@gmail.com>
2023-05-13 21:11:43 +0200

    final superLearne for regression

--- Code/superLearner.py
+++ Code/superLearner.py
@@ -1,4 +1,4 @@
-from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.estimator_checks import check_estimator
 from sklearn.model_selection import train_test_split
@@ -8,13 +8,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import linear_model
 from sklearn import neighbors
 from sklearn import datasets
+from sklearn import metrics
 import matplotlib.pyplot as plt
 from scipy import optimize
 from pandas.plotting import scatter_matrix
 import numpy as np
 import pandas as pd 
 
-class SuperLearner(BaseEstimator, RegressorMixin):
+class SuperLearner(BaseEstimator, RegressorMixin, ClassifierMixin):
     
     def __init__(self, base_estimators, meta_learner = None):
         self.base_estimators = base_estimators.values()
@@ -41,7 +42,10 @@ class SuperLearner(BaseEstimator, RegressorMixin):
         #df.rename(columns={last_column_index: 'y'}, inplace=True)
         #names = {i : estimator.__class__.__name__ for i, estimator in enumerate(self.base_estimators)}
         #df.rename(columns=names, inplace=True)
-        #print(df.head())
+        ##print(df.head())
+        #
+        #scatter_matrix(df, alpha = 0.2,  figsize = (6, 6), diagonal = 'kde')
+        #plt.show(block=False)
         #print(" ")
         
         if self.meta_learner is None:
@@ -79,8 +83,6 @@ class SuperLearner(BaseEstimator, RegressorMixin):
             base_predictions[:, i] = estimator.predict(X)
             
         return np.dot(base_predictions, self.weights)
-   
-###########################################################################################################################à    
 
 def generate_data(features = 10, n = 1000, plot = False):
     
@@ -110,22 +112,32 @@ def generate_data(features = 10, n = 1000, plot = False):
 
         plt.show()
     
-    Y = np.dot(X, beta) + np.random.normal(0, 10, size = n)
+    Y = np.dot(X, beta) + np.random.normal(0, 40, size = n)
 
     return X, Y
 
 def main():
     
-    X, y = datasets.make_friedman1(1000)
-    #X, y = generate_data(features = 50, n = 600, plot = False)
+    np.random.seed(1)
+    #X, y = datasets.make_friedman1(5000)
+    #X, y = datasets.make_friedman2(5000)
+    #X, y = generate_data(features = 50, n = 1000, plot = False)
+    X, y = datasets.make_regression(n_samples=1000, n_features=10, n_informative=5, n_targets=1, bias=0.0, effective_rank=None, tail_strength=0.5, noise=0.0, shuffle=True, coef=False, random_state=None)
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    scaler = StandardScaler()
+    
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+    y_train = scaler.fit_transform(y_train.reshape(-1,1)).flatten()
+    y_test = scaler.fit_transform(y_test.reshape(-1,1)).flatten()
     
     library = {
         "ols": linear_model.LinearRegression(),
-        "elastic": linear_model.ElasticNetCV(n_alphas=200, l1_ratio=0.5),
-        "ridge": linear_model.RidgeCV(alphas=np.arange(0.1, 10, 0.1)),
-        "lars": linear_model.LarsCV(),
-        "lasso": linear_model.LassoCV(alphas=np.arange(0.1, 10, 0.1)),
+        "elastic": linear_model.ElasticNet(),
+        "ridge": linear_model.Ridge(),
+        "lars": linear_model.Lars(),
+        "lasso": linear_model.Lasso(),
         "knn": neighbors.KNeighborsRegressor()
     }
     
@@ -134,16 +146,21 @@ def main():
     superLeaner1.fit(X_train, y_train)
     y_pred = superLeaner1.predict(X_test)
     
-    superLeaner2 = SuperLearner(library, linear_model.LinearRegression(positive=True))
+    superLeaner2 = SuperLearner(library, linear_model.ElasticNetCV(positive=True, alphas=np.arange(0.01, 10.0, 0.01)))
     superLeaner2.fit(X_train, y_train)
     
     print(" ")
     print("R^2 without meta learner: ", superLeaner1.score(X_test, y_test))
     print("R^2 with meta learner: ", superLeaner2.score(X_test, y_test))
+    #print("MSE without meta learner: ", metrics.mean_squared_error(y_test, y_pred))
+    #print("MSE with meta learner: ", metrics.mean_squared_error(y_test, superLeaner2.predict(X_test)))
     print(" ")
     
     for i, estimator in enumerate(superLeaner1.base_estimators):
         print("R^2 for {name}: {score}".format(name = estimator.__class__.__name__, score = estimator.score(X_test, y_test)))
+        #print("MSE for {name}: {score}".format(name = estimator.__class__.__name__, score = metrics.mean_squared_error(y_test, estimator.predict(X_test))))
+        
+    #pause = input("Press the <ENTER> key to stop execution...")
     
 if __name__ == "__main__":
     main()
